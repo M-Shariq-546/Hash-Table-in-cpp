54fd31443b81b6159fcebc6724d2fe3b9d1f1706
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2022-10-09 15:23:22 +0500

    HASH TABLE

--- hash_table.cpp
+++ hash_table.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 class hashtable
 {
-    const int size = 10;
+    int size = 10;
     int arr[10];
     int count = 0;
     public:
@@ -23,20 +23,25 @@ class hashtable
         void add(int k , int v)
         {
             int x = k % size;
+                    //For Inserting value on an Empty index
                     if(arr[x] == -9999)
                     {
                         arr[x] = v;
                         count++;
                     }
                     else{
+                        //Linear Probing due to collision
                         for(int i=0 ; i<size ; i++)
                         {
-                            while(arr[x] == -9999)
+                            if(arr[x] != -9999)
                             {
-                                x++;
+                                while(arr[x + 1] == -9999)
+                                {
+                                    x++;
+                                }
+                                arr[x]  = v;
                             }
                         }
-                        arr[x] =  v;
                         count++;
                     }
         }
@@ -45,23 +50,48 @@ class hashtable
             int x = k % size;
             if(arr[x] != -9999)
             {
+                //Value Found 
                 cout<<"The value is : "<<arr[x]<<endl;
             }
             else{
+                //Value Not Found Exception
                 cout<<"No value Found"<<endl;
             }
         }
         void print()
         {
+            cout<<"Sr No."<<endl;
             for(int i=0 ; i<size ; i++)
             {
-                cout<<arr[i]<<"\n";
+                //Printing Values in regular way by showing their indeces 
+                cout<<i<<":- "<<arr[i]<<"\n";
+            }
+        }
+        void deleteElement(int k)
+        {
+            int x = k % size;
+            if(arr[x] == -9999)
+            {
+                //Already Empty space Exception
+                cout<<"The space is Already Empty . Nothing found ."<<endl;
+            }
+            else{
+                int y = arr[x];
+                arr[x] = -9999;
+                cout<<"The element \""<<y<<"\" is successfully deleted."<<endl;
+                count--;
             }
         }
         void percentage()
         {
+            //Weight of a hash table according to values
+            cout<<"The Total values in array are : "<<count <<endl;
             float per = (float(count) / size) * 100;
             cout<<"The Percentage Weight of this array is : "<<per<<"%"<<endl;
+            if(per >= 50)
+            {
+                size * 2;
+            }
         }
 };
 int main()
@@ -71,9 +101,15 @@ int main()
     ht.initialArray();
     ht.add(20 , 5);
     ht.add(21 , 4);
+    ht.add(11 , 8);
     ht.add(35 , 6);
     ht.print();
     ht.getValue(35);
     ht.getValue(38);
+    ht.getValue(11);
     ht.percentage();
+    ht.deleteElement(21);
+    ht.print();
+    ht.percentage();
+    return 0;
 }
